bf82d1d010467e814c8b8db71185138bf65f52d8
vlatrof <gthbttr@gmail.com>
2022-12-04 23:45:49 +0300

    dev

--- app/src/main/java/com/vlatrof/androidrxjavademo/MainActivity.kt
+++ app/src/main/java/com/vlatrof/androidrxjavademo/MainActivity.kt
@@ -5,13 +5,14 @@ import android.text.method.ScrollingMovementMethod
 import androidx.appcompat.app.AppCompatActivity
 import com.vlatrof.androidrxjavademo.databinding.ActivityMainBinding
 import io.reactivex.Observable
-import io.reactivex.Single
 import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val disposeBag = CompositeDisposable()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,54 +20,43 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.tvLogs.movementMethod = ScrollingMovementMethod()
-        binding.btnClearLogs.setOnClickListener { clearLogs() }
-        binding.btnJustClick.setOnClickListener { writeToLog("Main thread: button clicked") }
-        binding.btnStartComputationObservable.setOnClickListener { startComputationObservable() }
-        binding.btnStartComputationSingle.setOnClickListener { startComputationSingle() }
+        binding.btnStartComputation.setOnClickListener { onStartComputation() }
+        binding.btnJustClick.setOnClickListener { onButtonJustClick() }
+        binding.btnClearLogs.setOnClickListener { onClearLogs() }
     }
 
-    private fun startComputationSingle() {
-        writeToLog("Main thread: Computation single started")
-        val singleObservable = Single.create<Int> { singleEmitter ->
-            Thread.sleep(1000)
-            singleEmitter.onSuccess(1)
-        }
+    private fun onStartComputation() {
+        writeToLog("Background computation: started")
 
-        val singleDisposable = singleObservable
-            .subscribeOn(Schedulers.computation())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe({ successValue ->
-                writeToLog("Main thread: Computation single completed: $successValue")
-            }, {})
-    }
-
-    private fun startComputationObservable() {
-        writeToLog("Main thread: Computation observable started")
-        val observable = Observable.create { emitter ->
-            for (i in 0 until 5) {
+        val disposable = Observable.create { emitter ->
+            for (i in 0 until 10) {
                 Thread.sleep(1000)
                 emitter.onNext(i)
             }
             emitter.onComplete()
-        }
-
-        val disposable = observable
+        } // configuration chain:
+            .map { it * it }
             .subscribeOn(Schedulers.computation())
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe(
-                { onNextValue -> writeToLog("BG thread: computation observable: $onNextValue") },
+                { onNextValue -> writeToLog("Background computation: $onNextValue") },
                 {},
-                { writeToLog("Main thread: Computation observable completed") }
+                { writeToLog("Background computation: completed") }
             )
     }
 
-    private fun clearLogs() {
+    private fun onButtonJustClick() {
+        writeToLog("Main thread: button clicked")
+    }
+
+    private fun onClearLogs() {
         binding.tvLogs.text = ""
     }
 
     private fun writeToLog(message: String) {
-        // to TextView
-        val nextLine = if (binding.tvLogs.text.isEmpty()) "" else "\n"
-        binding.tvLogs.append(nextLine + message)
+        binding.tvLogs.let {
+            if (it.text.isNotEmpty()) it.append("\n")
+            it.append(message)
+        }
     }
 }
